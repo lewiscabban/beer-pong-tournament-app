231e823e9b5b822af111d01f85d9dc1902177bf6
lewiscabban <lou231919@hotmail.com>
2022-08-16 20:43:34 +1000

    adding graphql to backend

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,31 @@
-from typing import Union
+import strawberry
 
+from typing import List
 from fastapi import FastAPI
-from pydantic import BaseModel
+from strawberry.fastapi import GraphQLRouter
+
+@strawberry.type
+class Player:
+    user_name: str
+    first_name: str
+    last_name: str
+    password: str
+
+def get_player() -> Player:
+    return Player(
+        user_name="chatooka",
+        first_name="cha",
+        last_name="tooka",
+        password="rat",
+    )
+
+@strawberry.type
+class Query:
+    players: List[Player] = strawberry.field(resolver=get_player)
+
+schema = strawberry.Schema(Query)
+
+graphql_app = GraphQLRouter(schema)
 
 app = FastAPI()
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+app.include_router(graphql_app, prefix="/graphql")
